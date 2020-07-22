15af24aadcee840d5081703cc49d9c6c14d578b6
Igor Fernandes <fdesigor@gmail.com>
2020-07-21 22:55:47 -0300

    feat(Cadastro): Tela de cadastro de professor

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Cadastro from './pages/Cadastro';
 
 function Routes(){
     return (
         <Switch>
             <Route path='/' exact component={Login} />
             <Route path='/dashboard' component={Dashboard} />
+            <Route path='/cadastro' component={Cadastro} />
         </Switch>
     );
 }
